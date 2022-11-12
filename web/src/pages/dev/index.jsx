c7f4565bbbb0f29949ef6e6793096ae995ea423c
Connor Emerick <temeculous@Connors-MacBook-Pro.local>
2022-11-11 19:21:07 -0500

    navbar

--- web/src/pages/dev/index.jsx
+++ web/src/pages/dev/index.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import PrimarySearchAppBar from "../../../src/navigation";
 
 export default function Dev() {
-    return (
-        <div>
-            <h1>Dev Page</h1>
-        </div>
-    )
+  return (
+    <div>
+      <PrimarySearchAppBar />
+      <h1>Dev Page</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo dicta,
+        voluptas magni quaerat fugit sapiente excepturi corporis! Voluptate
+        excepturi aspernatur, nam fugiat, necessitatibus obcaecati iure
+        dignissimos quam nobis sapiente aliquid!
+      </p>
+    </div>
+  );
 }
